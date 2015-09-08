61eb34f2a0acdf8c1cf8e5ef6e186dc614d41738
Dan Ubilla <dan.ubilla@gmail.com>
2015-09-08 00:15:00 -0400

    Stub out picks table

--- app/controllers/round_entries_controller.rb
+++ app/controllers/round_entries_controller.rb
@@ -1,5 +1,5 @@
 class RoundEntriesController < ApplicationController
   def show
-    @round_entry = RoundEntry.includes({entry: {player: :user}}, :round).find(params[:id])
+    @round_entry = RoundEntry.includes({entry: {player: :user}}, :round, {picks: [:opponent, game: [:away_opponent, :home_opponent]]}).find(params[:id])
   end
 end
