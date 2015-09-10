b3f93431261032f55f9e60d8e9e842ff271e9c66
Dan Ubilla <dan.ubilla@gmail.com>
2015-09-09 22:44:10 -0400

    Add validations to Pick

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -2,4 +2,15 @@ class Pick < ActiveRecord::Base
   belongs_to :round_entry
   belongs_to :game
   belongs_to :opponent
+
+  validates :round_entry, presence: true
+  validates :game, presence: true
+  validates :opponent, presence: true
+  validate :opponent_in_game
+
+  private
+
+  def opponent_in_game
+    errors.add(:base, 'Opponent must be in game') unless [game.home_opponent, game.away_opponent].include? opponent
+  end
 end
